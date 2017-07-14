9cbbe59adf07e8fea5554dee3b33756d1c224aac
ooyy67 <ooyy67@gmail.com>
2017-07-14 12:59:48 +0800

    init commit

---
+++ StudentManagementLibrary/StudentManagementLibrary/StudentManagementLibrary/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagementLibrary
+{
+    public class Student
+    {
+        public string name;
+        public string phone;
+        public string address;
+    }
+
+    public class Pokeman
+    {
+        public string name;
+        public string phone;
+        public string address;
+    }
+}
